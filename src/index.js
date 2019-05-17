7f5cc8acc61179a0bdb2c35ed3cbc40806efaa90
leny <info@flatland.be>
2019-05-17 21:19:04 +0200

    Add tooling for build

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* leny/gadmwaye
+ *
+ * /src/index.js - Main entrypoint
+ *
+ * coded by leny@BeCode
+ * started at 17/05/2019
+ */
+
+// eslint-disable-next-line no-console
+console.log("hello, world!");
